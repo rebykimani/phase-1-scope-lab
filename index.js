81aa0260eb14c7b724a8b001951e519abb5e3ddb
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-22 09:44:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+const LeastFavoriteCustomer = "Rebecca";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "bob";
+  }
